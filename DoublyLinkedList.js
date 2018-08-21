3faacaeb794f8c53d16462e3399cbb9aa6aacd73
dhanvinshah <dhanvin.shah@gmail.com>
2018-08-20 21:01:35 -0400

    Doubly Linked List with delete

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -12,8 +12,52 @@ class DoublyLinkedList {
         this.head = null;
     }
 
+    search(value) {
+        let currentNode = this.head;
+        while(currentNode) {
+            if(currentNode.data === value) return currentNode;
+            else currentNode = currentNode.next;
+        }
+
+        return null;
+    }
+
+    delete(value) {
+        let currentNode = this.head;
+        if(currentNode.data == value) {
+            this.head = currentNode.next;
+            this.head.previous = null;
+        }
+        else {
+            let previousNode = currentNode.previous;
+            let nextNode = currentNode.next;
+            while(currentNode) {
+                if(currentNode.data === value) {
+                    previousNode.next = nextNode;
+                    nextNode.previous = previousNode;
+                    this._length--;
+                    break;
+                }
+                else {
+                    currentNode = currentNode.next;
+                    previousNode = currentNode.previous;
+                    nextNode = currentNode.next;
+                }
+            }
+        }
+    }
+
     addToEnd(value) {
         let node = new Node(value);
+        let currentNode = this.head;
+
+        while(currentNode.next) {
+            currentNode = currentNode.next;
+        }
+
+        currentNode.next = node;
+        node.previous = currentNode;
+        this._length++;
     }
 
     addToHead(value) {
@@ -38,5 +82,15 @@ let doublyLinkedList = new DoublyLinkedList();
 doublyLinkedList.addToHead(100);
 doublyLinkedList.addToHead(200);
 doublyLinkedList.addToHead(300);
+doublyLinkedList.addToEnd(400);
+doublyLinkedList.addToEnd(500);
+
+// let node = doublyLinkedList.search(500);
+// console.log(node);
+
+// node = doublyLinkedList.search(1);
+// console.log(node);
+
+doublyLinkedList.delete(100);
 
 console.log(doublyLinkedList);
